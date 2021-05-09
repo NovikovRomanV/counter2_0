8a43e662fda1d3e6dc7a8c76f9bf0a9fa87111a0
NovikovRomanV <rimm19845@gmail.com>
2021-05-09 15:10:55 +0300

    create counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState(0)
+    const [maxValueCounter, setMaxValueCounter] = useState(0)
+
+    useEffect(() => {
+        let valueLocalStorage = localStorage.getItem("valueKey")
+        if (valueLocalStorage) {
+            setValue(JSON.parse(valueLocalStorage))
+        }
+    }, [])
+    useEffect(() => {
+        localStorage.setItem("valueKey", JSON.stringify(value))
+    }, [value])
+
+    const countInc = () => {
+        if(maxValueCounter>value)setValue(value + 1)
+    }
+
+
+    const countRest = () => {
+        setValue(0)
+    }
+
+    // const setValueLocolStorage = () => {
+    //     localStorage.setItem("valueKey", JSON.stringify(value))
+    // }
+
+    // const getValueLocolStorage = () => {
+    //     let valueLocalStorage = localStorage.getItem("valueKey")
+    //     if (valueLocalStorage) {
+    //         setValue(JSON.parse(valueLocalStorage))
+    //     }
+    // }
+
+    const startValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const startValue = e.currentTarget.value
+        if (startValue ) {
+            setValue(JSON.parse(startValue))
+        }
+    }
+
+    const maxValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const maxValue = e.currentTarget.value
+        if (maxValue) {
+            setMaxValueCounter(JSON.parse(maxValue))
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="count-left">
+                    <div>
+                        <div>
+                            Max value: <input type={"number"} onChange={maxValue}></input>
+                        </div>
+                        <div>
+                            Start value: <input type={"number"} onChange={startValue}></input>
+                        </div>
+                    </div>
+                    <button>set</button>
+                    {/*<button onClick={getValueLocolStorage}>get</button>*/}
+                </div>
+                <div className="count-right">
+                    <span>{value}</span>
+                    <div>
+                        <button onClick={countInc}>inc</button>
+                        <button onClick={countRest}>reset</button>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
+function setValue(arg0: number) {
+    throw new Error('Function not implemented.');
+}
+
